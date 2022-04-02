9ccf0992a4f4591681d1f575623958b552e958a7
Luis Macias <el.lobo.i@gmail.com>
2022-04-01 20:52:47 -0600

    primer commit con la arquitectura que genera cookiecutter como base para el proyecto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Preparando el proyecto de Titanic para pre-producciónde la experimentación al desarrollo',
+    author='Equipo 3',
+    license='',
+)
