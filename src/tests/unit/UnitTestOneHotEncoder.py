8c0bc3386f0bf1f8875fedad945e74ca2f7c054d
Luis Macias <el.lobo.i@gmail.com>
2022-04-20 00:00:41 -0500

    ya tomando el OneHotencoding de Cesar para la prueba unitaria, todo bien

--- src/tests/unit/UnitTestOneHotEncoder.py
+++ src/tests/unit/UnitTestOneHotEncoder.py
@@ -1,17 +1,39 @@
+# import sys
+# sys.path.insert(0, "../../features")
+# sys.path.insert(1, "/c/Tutoriales/Titanic_Pipeline_MLOps_Eq3/src/features")
+# sys.path.append("/c/Tutoriales/Titanic_Pipeline_MLOps_Eq3/src/features")
+# sys.path.append("features")
+
 from sre_constants import CATEGORY_UNI_LINEBREAK
 import pandas as pd
 import pytest
+from src.features.one_hot_encoder import OneHotEncoder
 
 
-def obtener_datos_one_hot_encoder(df):
+def obtener_datos_one_hot_encoder():
     titles = ["Mrs", "Mr", "Miss", "Master", "Other"]
-    titles_len = len(titles)
-    variables = ["title"]
-    df = pd.DataFrame(titles, columns=["title"])
-    return [titles_len, df, variables]
+    # la longitud es menos 1 columna, porque el OneHotEncodig no toma la columna de target
+    titles_len = len(titles) - 1
+    titles_variables = ["title"]
+    titles_df = pd.DataFrame(titles, columns=titles_variables)
+
+    cabins = ["B5", "C22", "E12", "D7", "A36", "C101", "C62", "B35", "A23"]
+    cabins_variables = ["cabin"]
+    cabins_len = len(cabins) - 1
+    cabins_df = pd.DataFrame(cabins, columns=cabins_variables)
+
+    return [
+        (titles_len, titles_df, titles_variables),
+        (cabins_len, cabins_df, cabins_variables),
+    ]
 
 
 @pytest.mark.parametrize("result, df, variables", obtener_datos_one_hot_encoder())
-def test_one_hot_encoding(len_one_hot, df, variables):
+def test_one_hot_encoding(result, df, variables):
     # traer el OneHotEncoding de Cesar y hacer las pruebas, debe pasar de 1 columna a 5
-    assert df.shape[1] == len_one_hot
+    one_hot = OneHotEncoder(variables)
+    one_hot.fit(df)
+    df = one_hot.transform(df)
+    print(f"df.shape = {df.shape}")
+    print(f"df.columns = {df.columns}")
+    assert len(df.columns) == result
