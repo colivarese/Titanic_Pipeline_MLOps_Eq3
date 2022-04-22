aef178ada0b5f41bbf82f246c14069e634bdc8e2
colivarese <ces.oe97@gmail.com>
2022-04-21 21:22:17 -0500

    Se mueve archivo cabin_only_letter.py

---
+++ src/features/cabin_only_letter.py
@@ -0,0 +1,19 @@
+import re
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from typing import List
+
+class CabinOnlyLetter(BaseEstimator, TransformerMixin):
+
+def __init__(self, column: str):
+    self.column = column
+
+def fit(self, x: pd.DataFrame):
+    pass
+
+def transform(self, X: pd.DataFrame):
+
+    X[self.column] = [''.join(re.findall("[a-zA-Z]+", row)) if type(row) == str else row for row in X[self.column]]
+
+    return X
